e3f341fa59c2b2a3c35b51fae6f2a59c379367a3
Markus Åkesson <macke.95_06@hotmail.com>
2021-07-23 15:35:45 +0200

    Use custom panic handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,35 @@
 #![no_std]
 #![feature(asm)]
 #![feature(global_asm)]
-
-extern crate panic_halt;
+#![feature(panic_info_message)]
 
 pub mod arch;
+pub mod interrupt;
 pub mod klog;
 pub mod mem;
 pub mod page;
+pub mod plic;
 pub mod symbols;
 pub mod trap;
 pub mod uart;
+
+/// Panic handler
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    println!("hart {} aborting: ", arch::riscv::thread_pointer());
+    if let Some(p) = info.location() {
+        println!(
+            "line {}, file {}: {}",
+            p.line(),
+            p.file(),
+            info.message().unwrap()
+        );
+    } else {
+        println!("no information available.");
+    }
+    loop {
+        unsafe {
+            riscv::asm::wfi();
+        }
+    }
+}
